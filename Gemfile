9be0a3dcee0a33121cc2266046f49ed494f27a7f
Ross McKee <rossrobertmckee@gmail.com>
2015-05-15 23:40:57 +0000

    Linking and a whole lotta other stuff...

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.4'
 
+gem 'simple_form'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,9 @@
 class QuotesController < ApplicationController
 	def index
-		@quote = Quote.order("Random()").first
+		@quote = Quote.order("RANDOM()").first
+	end
+
+	def new
+		@quote = Quote.new
 	end
 end
