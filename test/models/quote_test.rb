042f387b2623c79a127da268ed9f368ab45cbfd6
Ross McKee <rossrobertmckee@gmail.com>
2015-06-02 17:03:00 +0000

    Factory Girl

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do
-		quote = Quote.create(:author => 'Ross McKee', :saying => 'Soakin Wet!')
+		quote = FactoryGirl.create(:quote, :author => 'Ross McKee')
 		expected = 'RM#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual	
