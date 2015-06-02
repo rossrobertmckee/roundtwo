898193608ed38ae1c5f029be78130606ac23d0e3
Ross McKee <rossrobertmckee@gmail.com>
2015-06-02 01:00:50 +0000

    Splurty Test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Ross McKee', :saying => 'Soakin Wet!')
+		expected = 'RM#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual	
+	end
+
 end
